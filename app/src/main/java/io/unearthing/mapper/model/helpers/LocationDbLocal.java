614b6a1c00b1a9e221e47eb7600b2efcf3b27e8b
billybonks <sebastienstettler@gmail.com>
2016-02-27 14:16:30 +0800

    get trips in DSC order

--- app/src/main/java/io/unearthing/mapper/model/helpers/LocationDbLocal.java
+++ app/src/main/java/io/unearthing/mapper/model/helpers/LocationDbLocal.java
@@ -50,7 +50,7 @@ public class LocationDbLocal implements LocationDb {
     public Cursor getTrips(){
         SQLiteDatabase db = mTripTable.getReadableDatabase();
         //String query = "SELECT t.title as title, count(l._id) as count from trip t INNER JOIN location l on t._id = l.trip WHERE t._id = 2";
-        String query = "SELECT _id, title from trip";
+        String query = "SELECT _id, title from trip order by _id DESC";
         return db.rawQuery(query,null);
     }
 
