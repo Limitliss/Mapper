727acaf8d609cb73eea6b237a3baa6de626c85c7
billybonks <sebastienstettler@gmail.com>
2016-03-04 00:54:05 +0800

    Move service code to fragment

--- app/src/main/java/io/unearthing/mapper/ui/fragments/TripListFragment.java
+++ app/src/main/java/io/unearthing/mapper/ui/fragments/TripListFragment.java
@@ -17,12 +17,23 @@
 
 package io.unearthing.mapper.ui.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
@@ -37,9 +48,11 @@ import android.widget.SimpleCursorAdapter;
 import java.util.List;
 
 import io.unearthing.mapper.CloudentBuilder;
+import io.unearthing.mapper.ILocationService;
 import io.unearthing.mapper.R;
 import io.unearthing.mapper.model.Trip;
 import io.unearthing.mapper.model.helpers.LocationDbLocal;
+import io.unearthing.mapper.services.LocationService;
 import io.unearthing.mapper.services.UploadTrip;
 import io.unearthing.mapper.ui.TripAdapter;
 import io.unearthing.mapper.ui.activities.MapActivity;
@@ -47,6 +60,23 @@ import io.unearthing.mapper.ui.activities.MapActivity;
 public class TripListFragment extends Fragment {
 
     List<Trip> mTrips;
+
+    private final String mServiceName = "io.unearthing.mapper.services.LocationService";
+    private boolean mServiceRunning = false;
+    private ILocationService mLocationService;
+
+    private ServiceConnection mConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mLocationService = ILocationService.Stub.asInterface(service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mLocationService = null;
+        }
+    };
 
     public TripListFragment() {
 
@@ -67,5 +97,86 @@ public class TripListFragment extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(adapter);
         return view;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        bindFab();
+        boolean serviceStarted = isServiceStarted();
+
+        if(serviceStarted){
+            bindService();
+        }
+        setServiceRunning(serviceStarted);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(isServiceRunning()){
+            getActivity().unbindService(mConnection);
+        }
+    }
+
+    public void bindFab(){
+        FloatingActionButton button = (FloatingActionButton)getActivity().findViewById(R.id.service_control_button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(isServiceRunning()){
+                    stopService();
+                }else{
+                    startService();
+                }
+            }
+        });
+    }
+    public boolean isServiceRunning() {
+        return mServiceRunning;
+    }
+
+    public void setServiceRunning(boolean serviceRunning) {
+        FloatingActionButton button = (FloatingActionButton)getActivity().findViewById(R.id.service_control_button);
+        Drawable gfx;
+        if(serviceRunning){
+            gfx = getActivity().getDrawable(R.drawable.ic_media_pause);
+        }else{
+            gfx = getActivity().getDrawable(R.drawable.ic_media_play);
+        }
+        button.setImageDrawable(gfx);
+        this.mServiceRunning = serviceRunning;
+    }
+
+
+    private void startService(){
+        Intent intent = new Intent(getActivity(), LocationService.class);
+        getActivity().startService(intent);
+        this.bindService();
+        setServiceRunning(true);
+    }
+
+    private void bindService(){
+        Intent intent = new Intent(getActivity(), LocationService.class);
+        getActivity().bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void stopService(){
+        Intent intent = new Intent(getActivity(), LocationService.class);
+        getActivity().stopService(intent);
+        getActivity().unbindService(mConnection);
+        setServiceRunning(false);
+    }
+
+
+    private boolean isServiceStarted(){
+        ActivityManager manager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo service: manager.getRunningServices(Integer.MAX_VALUE)){
+            if(mServiceName.equals(service.service.getClassName())){
+                return true;
+            }
+        }
+        return false;
     }
 }
