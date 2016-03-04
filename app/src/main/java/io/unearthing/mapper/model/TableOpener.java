7acf1642f74ba3915ec40c9d1fbe66a9c5d16f2f
billybonks <sebastienstettler@gmail.com>
2016-03-04 19:20:32 +0800

    define trip summary

--- app/src/main/java/io/unearthing/mapper/model/TableOpener.java
+++ app/src/main/java/io/unearthing/mapper/model/TableOpener.java
@@ -11,7 +11,7 @@ import android.provider.BaseColumns;
 public  class TableOpener extends SQLiteOpenHelper {
 
 
-    public static final int DATABASE_VERSION = 8;
+    public static final int DATABASE_VERSION = 9;
     public static final String DATABASE_NAME = "LocationStore.db";
 
     public static abstract class LocationTableContract implements BaseColumns {
@@ -25,7 +25,7 @@ public  class TableOpener extends SQLiteOpenHelper {
         public static final String COLUMN_NAME_BEARING = "bearing";
         public static final String COLUMN_NAME_ALTITUDE = "altitude";
         public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
-        public static final String COLUMN_NAME_TRIP  = "trip";
+        public static final String COLUMN_NAME_TRIP  = "trip_id";
     }
 
     public static abstract class TripTableContract implements BaseColumns {
@@ -34,6 +34,28 @@ public  class TableOpener extends SQLiteOpenHelper {
         public static final String COLUMN_NAME_END = "end";
         public static final String COLUMN_NAME_TITLE = "title";
     }
+
+    public static abstract class TripSummaryTableContract implements BaseColumns {
+        public static final String TABLE_NAME = "trip_summary";
+        public static final String COLUMN_NAME_LOCATIONS = "locations";
+        public static final String COLUMN_NAME_SPEED = "speed";
+        public static final String COLUMN_NAME_DISTANCE = "distance";
+        public static final String COLUMN_NAME_DURATION = "duration";
+        public static final String COLUMN_NAME_TRIP = "trip_id";
+    }
+
+    private static final String SQL_CREATE_TRIP_SUMMARY_TABLE =
+            "CREATE TABLE " + TripSummaryTableContract.TABLE_NAME   + "(" +
+                    TripSummaryTableContract._ID                    + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    TripSummaryTableContract.COLUMN_NAME_TRIP       +" INTEGER," +
+                    TripSummaryTableContract.COLUMN_NAME_LOCATIONS  +" INTEGER," +
+                    TripSummaryTableContract.COLUMN_NAME_SPEED      +" REAL," +
+                    TripSummaryTableContract.COLUMN_NAME_DISTANCE   +" REAL," +
+                    TripSummaryTableContract.COLUMN_NAME_DURATION   +" REAL," +
+                    ")";
+
+    private static final String SQL_DELETE_TRIP_SUMMARY_TABLE =
+            "DROP TABLE IF EXISTS " + LocationTableContract.TABLE_NAME;
 
     private static final String SQL_CREATE_LOCATION_TABLE =
             "CREATE TABLE " + LocationTableContract.TABLE_NAME  + "(" +
@@ -69,11 +91,13 @@ public  class TableOpener extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_LOCATION_TABLE);
         db.execSQL(SQL_CREATE_TRIP_TABLE);
+        db.execSQL(SQL_CREATE_TRIP_SUMMARY_TABLE);
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(SQL_DELETE_LOCATION_TABLE);
         db.execSQL(SQL_DELETE_TRIP_TABLE);
+        db.execSQL(SQL_DELETE_TRIP_SUMMARY_TABLE);
         onCreate(db);
     }
 
